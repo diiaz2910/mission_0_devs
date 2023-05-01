0014ed195dcd3412c3e79921ceed446698c41127
Ricardo <diiaz2910@gmail.com>
2023-05-01 17:13:48 +1200

    F_COMMIT

---
+++ resources/views/livewire/wireframe.blade.php
@@ -0,0 +1,69 @@
+<nav class="bg-white"> {{-- NAV BAR COLOR --}}
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-between">
+
+
+        {{-- MOBILE MENU --}}
+        <div class="absolute inset-y-0 right-0 flex items-center sm:hidden">
+
+            {{-- BURGER MENU --}}
+            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+            
+        </div>
+
+
+        {{-- LOGO --}}
+        <div class="absolute inset-y-0 left-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+            {{-- LOGO --}}
+            <div class="flex flex-shrink-0 items-center">
+            
+            {{-- LOGO IMAGE --}}
+            <div>
+                <div class="w-12 h-12 block lg:hidden bg-gray-400 rounded-full"></div>
+                <div class="w-12 h-12 hidden lg:block bg-gray-400 rounded-full"></div>
+            </div>
+
+            </div>
+
+          {{-- TEXT MY COMPANY--}}
+          <div class="flex flex-shrink-0 items-center">
+            <a href="#" class="text-black hover:text-gray-400 rounded px-5 py-2 text-sm font-medium">My Company</a>
+          </div>
+
+            
+          
+        </div>
+
+
+        {{-- CATEGORIES --}}
+        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {{-- Categories --}}
+            <div class="hidden sm:ml-6 sm:block">
+                <div class="flex space-x-4">
+                <a href="#" class="text-black hover:bg-gray-700 hover:text-white px-7 py-2 text-sm font-medium">Menu1</a>
+                <a href="#" class="text-black hover:bg-gray-700 hover:text-white px-7 py-2 text-sm font-medium">Menu2</a>
+                <a href="#" class="text-black hover:bg-gray-700 hover:text-white px-7 py-2 text-sm font-medium">Menu3</a>
+                <a href="#" class="bg-white hover:bg-gray-200 border-black text-black border px-7 py-2 text-sm font-medium" aria-current="page">Login</a>
+                </div>
+            </div>
+        </div>
+
+
+      </div>
+    </div>
+
+
+
+    <!-- Mobile menu, show/hide based on menu state. 
+    <div class="sm:hidden" id="mobile-menu">
+        <div class="space-y-1 px-2 pb-3 pt-2">
+            <p class="text-5xl">
+                Lorem ipsum dolor sit amet
+            </p>
+        </div>
+    </div>
+    -->
+</nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,87 @@
+<x-app-layout>
+
+
+<div class="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-29">
+
+    {{-- SEARCH BAR AND TEXT --}}
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+
+        <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-1">
+            {{-- LOREM TITLE --}}
+            <div class="max-w-xl lg:max-w-lg">
+                <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Lorem ipsum dolor sit, amet</h2>
+            </div>
+            <dl class="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+            </dl>
+        </div>
+
+        {{-- SEARCH BAR --}}
+        <div class="mt-14 flex max-w-md justify-center">
+            <label for="email-address" class="sr-only ">Search</label>
+            <input id="search-bar" name="search" required class="min-w-max h-12 flex-auto border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="Search Bar">
+            <button type="submit" class=" bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Search</button>
+        </div>
+
+    </div>
+
+
+    {{-- GRADIET BG --}}
+    <div class="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6" aria-hidden="true">
+        <div class="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+    </div>
+
+
+</div>
+
+
+<div class="bg-gray-100">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+        <h2 class="text-2xl font-bold text-gray-900">Collections</h2>
+  
+        <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+          <div class="group relative">
+            <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+              <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg" alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." class="h-full w-full object-cover object-center">
+            </div>
+            <h3 class="mt-6 text-sm text-gray-500">
+              <a href="#">
+                <span class="absolute inset-0"></span>
+                necessitatibus reiciendis? Maiores facilis nostrum iste quidem amet vel enim aliquid adipisci.
+              </a>
+            </h3>
+            <p class="text-base font-semibold text-gray-900">Lorem ipsum dolor sit amet consectetur</p>
+          </div>
+          <div class="group relative">
+            <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+              <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg" alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." class="h-full w-full object-cover object-center">
+            </div>
+            <h3 class="mt-6 text-sm text-gray-500">
+              <a href="#">
+                <span class="absolute inset-0"></span>
+                necessitatibus reiciendis? Maiores facilis nostrum iste quidem amet vel enim aliquid adipisci.
+              </a>
+            </h3>
+            <p class="text-base font-semibold text-gray-900">Lorem ipsum dolor sit amet consectetur</p>
+          </div>
+          <div class="group relative">
+            <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+              <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg" alt="Collection of four insulated travel bottles on wooden shelf." class="h-full w-full object-cover object-center">
+            </div>
+            <h3 class="mt-6 text-sm text-gray-500">
+              <a href="#">
+                <span class="absolute inset-0"></span>
+                necessitatibus reiciendis? Maiores facilis nostrum iste quidem amet vel enim aliquid adipisci.
+              </a>
+            </h3>
+            <p class="text-base font-semibold text-gray-900">Lorem ipsum dolor sit amet consectetur</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+
+
+</x-app-layout>
+
